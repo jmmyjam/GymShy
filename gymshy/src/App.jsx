7c2e0bbc3eb0e02d4807a56cb94092df92675999
jmmyjam <jlee51@scu.edu>
2026-03-05 10:51:55 -0800

    Add Vite

---
+++ gymshy/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-600">GymShy is working!</h1>
+    </div>
+  )
+}
