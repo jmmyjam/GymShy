b2040dc9bf0027df5f7be42c3e476c3b25bb2bdb
jmmyjam <jlee51@scu.edu>
2026-03-05 17:02:50 -0800

    Add more API call functions

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from database import allequipment, user_equipment
+from database import Equipment, allequipment, user_equipment
 
 app = FastAPI()
 
 @app.get("/")
 async def greet():
-    return "Welcome to Gymmy"
+    return {
+            "message": "Welcome to Gymmy"
+            }
 
 @app.get("/allequipment")
 async def get_all_equipment():
@@ -16,7 +18,9 @@ async def get_equipment_id(name: str):
     for item in allequipment:
         if item.name.lower() == name.lower().replace(" ", ""):
             return item
-    return "item not found"
+    return {
+            "message": "Not found"
+            }
 
 @app.get("/myequipment")
 async def get_user_equipment():
@@ -29,4 +33,34 @@ async def add_item(name: str):
             if item not in user_equipment:
                 user_equipment.append(item)
             return user_equipment
-    return "item not found"
+    return {
+            "message": "Not found"
+            }
+
+@app.put("/allequipment")
+async def update(name: str, item: Equipment):
+    for i in range(len(allequipment)):
+        if allequipment[i].name.lower().replace(" ", "") == name.lower().replace(" ", ""):
+            allequipment[i] = item
+            return {
+                    "message": "Item updated successfully",
+                    "update": allequipment[i]
+                    }
+    return {
+            "message": "Not found"
+            }
+
+@app.delete("/myequipment")
+async def delete(name: str):
+    if len(user_equipment) == 0:
+        return "Empty list nothing deleted"
+    for i in range(len(user_equipment)):
+        if user_equipment[i].name.lower().replace(" ", "") == name.lower().replace(" ", ""):
+            deleted: Equipment = user_equipment.pop(i)
+            return {
+                    "message": "Item deleted successfully",
+                    "deleted": deleted
+                    }
+    return {
+            "message": "Not found"
+            }
